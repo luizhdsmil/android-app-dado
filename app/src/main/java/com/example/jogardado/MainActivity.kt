eb24c7e62dd52cbf53f6b067ce30e94dbf56b2dc
Your Name <luiz.henrique30@hotmail.com>
2019-10-27 21:16:06 -0300

    feature: adicionando imagens dos dados

--- app/src/main/java/com/example/jogardado/MainActivity.kt
+++ app/src/main/java/com/example/jogardado/MainActivity.kt
@@ -3,13 +3,39 @@ package com.example.jogardado
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val jogarDado: Button = findViewById(R.id.jogar_button)
-        jogarDado.text = "Luizinho"
+        val jogarDadoButton: Button = findViewById(R.id.jogar_button)
+        diceImage = findViewById(R.id.image_dice)
+        jogarDadoButton.setOnClickListener{
+            jogarDado()
+        }
+    }
+
+    private fun jogarDado() {
+
+        val number = Random().nextInt(6) + 1
+
+        val image_drawable = when (number) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(image_drawable)
+
     }
 }
